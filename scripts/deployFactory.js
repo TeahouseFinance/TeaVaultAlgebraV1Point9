f571a28121a369b12086b96dacfd0965c83e0d18
Venson <venson@teahouse.finance>
2024-10-30 19:13:53 +0800

    update deplo script

--- scripts/deployFactory.js
+++ scripts/deployFactory.js
@@ -20,8 +20,17 @@ const owner = loadEnvVar(process.env.OWNER, "No OWNER");;
 const poolFactory = loadEnvVar(process.env.POOL_FACTORY, "No POOL_FACTORY");
 
 async function main() {
-    const TeaVaultAlgebraV1Point9 = await ethers.getContractFactory("TeaVaultAlgebraV1Point9");
-    const beacon = await upgrades.deployBeacon(TeaVaultAlgebraV1Point9);
+    const VaultUtils = await ethers.getContractFactory("VaultUtils");
+    const vaultUtils = await VaultUtils.deploy();
+    console.log("VaultUtils deployed", vaultUtils.target);
+
+    const TeaVaultAlgebraV1Point9 = await ethers.getContractFactory("TeaVaultAlgebraV1Point9", {
+        libraries: { VaultUtils: vaultUtils.target },
+    });
+    const beacon = await upgrades.deployBeacon(TeaVaultAlgebraV1Point9, {
+            unsafeAllowLinkedLibraries: true,
+            unsafeAllow: ["delegatecall"],
+    });
     console.log("beacon deployed", beacon.target);
 
     const TeaVaultAlgebraV1Point9Factory = await ethers.getContractFactory("TeaVaultAlgebraV1Point9Factory");
